5486dddd749b9a271ecf03c8e36de1d17301c54e
MCavieres <maca.c.91@gmail.com>
2024-07-19 10:08:40 -0400

    cambio de colores de tabla y se agrega navbar y registro

--- routes/views.route.js
+++ routes/views.route.js
@@ -5,5 +5,8 @@ const router = Router();
 router.get("/", (req, res) => {
     res.render("home");
 });
+router.get("/register", (req, res) => {
+    res.render("register");
+});
 
 export default router;
