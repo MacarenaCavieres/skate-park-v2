688e36caefd58b4de4aa9453cd1b29ea37d8efe0
MCavieres <maca.c.91@gmail.com>
2024-07-22 13:28:44 -0400

    se agrega queries de base de datos

---
+++ database/queries.sql
@@ -0,0 +1,13 @@
+drop table if exists skatepark;
+
+CREATE TABLE skaters (
+    id SERIAL, 
+    email VARCHAR(50) NOT NULL, 
+    nombre VARCHAR(25) NOT NULL, 
+    password VARCHAR(25) NOT NULL, 
+    years_experience INT NOT NULL, 
+	specialty VARCHAR(50) NOT NULL, 
+    photo VARCHAR(255) NOT NULL, 
+    state BOOLEAN NOT NULL,
+	type_user INT default 2
+);
