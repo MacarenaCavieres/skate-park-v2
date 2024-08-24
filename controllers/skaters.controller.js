7d9e8b26e3768abdb19fc91fa05d970b79091b11
MCavieres <maca.c.91@gmail.com>
2024-08-23 21:57:19 -0400

    se ocupa el bcrypt para hashear la contraseña

--- controllers/skaters.controller.js
+++ controllers/skaters.controller.js
@@ -1,16 +1,19 @@
+import bcryptjs from "bcryptjs";
+import path from "path";
 import { Skater } from "../models/skaters.model.js";
 import { handleErrors } from "../database/errors.db.js";
-import path from "path";
 
 const __dirname = import.meta.dirname;
 const pathFile = path.join(__dirname, "../public/assets/imgs");
 
 const postOneSkater = async (req, res) => {
-    const { email, nombre, password, years_experience, specialty } = req.body;
+    const { email, nombre, password, rePassword, years_experience, specialty } = req.body;
     const { photo } = req.files;
 
     if (!email || !nombre || !password || !years_experience || !specialty || !photo)
         return res.status(409).json({ ok: false, msg: "Faltan campos" });
+
+    if (password !== rePassword) return res.status(400).json({ ok: false, msg: "Contraseñas no coinciden" });
 
     const name = `${nombre.split(" ").join("")}.jpg`;
     // const profileFoto = `${name}`;
@@ -22,8 +25,23 @@ const postOneSkater = async (req, res) => {
             }
         });
 
-        const data = await Skater.postOne(email, name, password, years_experience, specialty, name);
-        return res.redirect("regSuccess");
+        const salt = await bcryptjs.genSalt(10);
+        const hashPassword = await bcryptjs.hash(password, salt);
+
+        const data = await Skater.postOne(email, nombre, hashPassword, years_experience, specialty, name);
+        return res.render("regSuccessful", { data });
+    } catch (error) {
+        console.log(error);
+        const { code, msg } = handleErrors(error);
+        return res.status(code).json({ ok: false, msg });
+    }
+};
+
+const getAllSkaters = async (req, res) => {
+    try {
+        const data = await Skater.getAll();
+
+        res.render("home", { data });
     } catch (error) {
         console.log(error);
         const { code, msg } = handleErrors(error);
@@ -33,4 +51,5 @@ const postOneSkater = async (req, res) => {
 
 export const skatersController = {
     postOneSkater,
+    getAllSkaters,
 };
