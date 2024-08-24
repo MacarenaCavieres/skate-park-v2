c9dc2c36f42076a4025f2c3fa27737471f838be3
MCavieres <maca.c.91@gmail.com>
2024-08-23 21:57:42 -0400

    se modifica el largo de la contraseña

--- database/queries.sql
+++ database/queries.sql
@@ -4,7 +4,7 @@ CREATE TABLE skaters (
     id SERIAL, 
     email VARCHAR(50) NOT NULL, 
     nombre VARCHAR(25) NOT NULL, 
-    password VARCHAR(25) NOT NULL, 
+    password VARCHAR(100) NOT NULL, 
     years_experience INT NOT NULL, 
 	specialty VARCHAR(50) NOT NULL, 
     photo VARCHAR(255) NOT NULL, 
