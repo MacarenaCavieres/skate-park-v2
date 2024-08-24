c191fa0c5237783f8d6aab582db2cb98f5a207d1
MCavieres <maca.c.91@gmail.com>
2024-08-23 21:58:14 -0400

    se modifica para que al crear un skater retorne todo menos la contraseña

--- models/skaters.model.js
+++ models/skaters.model.js
@@ -2,7 +2,7 @@ import { pool } from "../database/connection.db.js";
 
 const postOne = async (email, nombre, password, years_experience, specialty, photo) => {
     const query = {
-        text: "insert into skaters (email,nombre,password,years_experience,specialty,photo) values ($1,$2,$3,$4,$5,$6) returning *;",
+        text: "insert into skaters (email,nombre,password,years_experience,specialty,photo) values ($1,$2,$3,$4,$5,$6) returning email,nombre,years_experience,specialty,photo;",
         values: [email, nombre, password, years_experience, specialty, photo],
     };
     const { rows } = await pool.query(query);
