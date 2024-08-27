ef003c264bb24f716b74af1d0c538dae0e1813f2
MCavieres <maca.c.91@gmail.com>
2024-08-27 15:40:02 -0400

    se hace data.js

---
+++ public/assets/js/data.js
@@ -0,0 +1,18 @@
+// const token = localStorage.getItem("token");
+
+// if (!token) {
+//     return (window.location.href = "/login");
+// }
+
+// const getProfile = async () => {
+//     try {
+//         const { data } = await axios.get("/data", {
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//             },
+//         });
+//         console.log(data);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
