d513692184d8dcddb7303bb666da235d5cb01080
MCavieres <maca.c.91@gmail.com>
2024-08-27 15:37:31 -0400

    modificacion de verificacion de token

--- middlewares/token.middleware.js
+++ middlewares/token.middleware.js
@@ -1,20 +1,24 @@
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 
-const secretKey = process.env.SK;
+const secretKey = process.env.secretKey;
 
 export const validateToken = (req, res, next) => {
-    console.log(req.headers);
-    const token = req.headers["authorization"] || req.query.token;
+    let token = req.headers.authorization;
+    console.log(req.tipo_usuario);
 
     if (!token) return res.status(401).json({ ok: false, msg: "Acceso denegado" });
 
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-            res.status(401).json({ ok: false, msg: "Acceso denegado, token expirado o incorrecto" });
-        } else {
-            req.email = user.email;
-            next();
-        }
-    });
+    token = token.split(" ")[1];
+
+    try {
+        const { email, type_user } = jwt.verify(token, secretKey);
+
+        req.email = email;
+        req.tipo_usuario = type_user;
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ ok: false, msg: "Token no válido" });
+    }
 };
