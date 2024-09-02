e17b6965cc5f03c3f5cd06f4e417d1a2ae24c4e8
MCavieres <maca.c.91@gmail.com>
2024-09-02 17:03:33 -0400

    se crea postOne

---
+++ models/admin.model.js
@@ -0,0 +1,26 @@
+import { pool } from "../database/connection.db.js";
+
+const postOne = async (username, email, password) => {
+    const query = {
+        text: "INSERT INTO admin (username, email, password) values ($1,$2,$3) returning *;",
+        values: [username, email.password],
+    };
+
+    const { rows } = pool.query(query);
+    return rows[0];
+};
+
+const findOne = async (email) => {
+    const query = {
+        text: "select * from admin where email = $1",
+        values: [email],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows[0];
+};
+
+export const Admin = {
+    postOne,
+    findOne,
+};
