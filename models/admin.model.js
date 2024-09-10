47acc7d382451b946e4fe3e2454a9d36f24182a7
MCavieres <maca.c.91@gmail.com>
2024-09-09 21:05:32 -0300

    se agrega el await en la promesa

--- models/admin.model.js
+++ models/admin.model.js
@@ -3,10 +3,10 @@ import { pool } from "../database/connection.db.js";
 const postOne = async (username, email, password) => {
     const query = {
         text: "INSERT INTO admin (username, email, password) values ($1,$2,$3) returning username, email;",
-        values: [username, email.password],
+        values: [username, email, password],
     };
 
-    const { rows } = pool.query(query);
+    const { rows } = await pool.query(query);
     return rows[0];
 };
 
