3aea59e462f2bde60948cc2f4e980fe56582464a
MCavieres <maca.c.91@gmail.com>
2024-09-23 18:49:59 -0300

    se agrega funcion para llamar a un admin por id

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -3,6 +3,9 @@ const username = document.querySelector("#username");
 const inputState = document.querySelector("#inputState");
 const stateSkater = document.querySelectorAll(".check");
 const registerAdmin = document.querySelector("#registerAdmin");
+const bntModal = document.querySelectorAll(".btn-update");
+const btnDelete = document.querySelectorAll(".btn-delete");
+const myModal = new bootstrap.Modal(document.getElementById("myModal"));
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(
@@ -83,5 +86,33 @@ registerAdmin.addEventListener("submit", async (e) => {
         return alert(error.response.data.msg);
     }
 });
+
+const infoAdmin = async (e) => {
+    const idAdmin = e.target.id;
+    try {
+        const { data } = await axios.get(`/admin/id/${idAdmin}`);
+        myModal.show();
+        console.log(data);
+
+        let username = updateForm.username;
+        let email = updateForm.email;
+
+        username.value = data.data.username;
+        email.value = data.data.email;
+    } catch (error) {
+        console.error(error);
+        return alert("Algo salió mal");
+    }
+
+    // const updateForm = document.querySelector("#updateForm");
+
+    // const username = updateForm.username.value;
+    // const email = updateForm.email.value;
+    // const password = updateForm.password.value;
+    // const rept_password = updateForm.rept_password.value;
+    // console.log(username, email, password, rept_password);
+};
+
+bntModal.forEach((btn) => btn.addEventListener("click", infoAdmin));
 
 getAdmin();
